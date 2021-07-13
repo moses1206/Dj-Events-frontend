81837350e115847f20b36587f8f1b49dd1fa2618
moses1206 <mose23@naver.com>
2021-07-13 20:16:12 +0900

    Add Header Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
 import styles from '../styles/Layout.module.css';
 
 export default function Layout({ title, description, keywords, children }) {
@@ -9,8 +11,9 @@ export default function Layout({ title, description, keywords, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
-
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
